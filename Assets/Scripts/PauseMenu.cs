ef86e5b6a096f03214b32f5ee251f35920527bd1
Leon <leon@nyoucomer.de>
2023-10-01 18:28:02 +0200

    options started

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] List<Button> buttonsToDisable;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +23,29 @@ public class PauseMenu : MonoBehaviour
     public void Pause() 
     { 
         Time.timeScale = 0;
+        pauseMenu.SetActive(true);
 
     }
 
     public void Unpause()
     {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
 
+    public void OpenOptions()
+    {
+        foreach (var button in buttonsToDisable)
+        {
+            button.interactable = false;
+        }
+    }
+
+    public void CloseOptions() 
+    {
+        foreach (var button in buttonsToDisable)
+        {
+            button.interactable = true;
+        }
     }
 }
