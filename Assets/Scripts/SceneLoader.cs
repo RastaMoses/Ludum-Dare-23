16b10c7bfb13db2a9904a5257ebe5200706d2655
Leon <leon@nyoucomer.de>
2023-10-01 18:10:00 +0200

    Added Pause Menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
