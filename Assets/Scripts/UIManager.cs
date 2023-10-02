7cb918be7e0bb9fa6fe274d06e84159e86f46c3e
Leon <leon@nyoucomer.de>
2023-10-02 02:29:10 +0200

    Health Animation UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System;
+using System.Threading;
 
 public class UIManager : MonoBehaviour
 {
@@ -32,38 +33,114 @@ public class UIManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI objectiveBonus;
     [SerializeField] TextMeshProUGUI bonusPoints;
     [Header("Health")]
-    [SerializeField] float debugHealth = 100;
     [SerializeField] float healthPicChangeThreshhold = 40;
     [SerializeField] Image healthBG;
     [SerializeField] List<Sprite> healthBGs;
     [SerializeField] Image healthPic;
     [SerializeField] List<Sprite> healthPics;
+    [SerializeField] float healthAnimTime = 2f;
     [SerializeField] public Image healthSlider;
+    [SerializeField] public Image healthSliderRed;
+    [SerializeField] AnimationCurve redHealthAnimCurve;
+    [SerializeField] float redHealthAnimSpeed;
     [SerializeField] Color lowHealthBar;
     [SerializeField] TextMeshProUGUI lifes;
+
+    [Header("Debug")]
+    [SerializeField] float debugHealth = 100f;
     //State
     List<GameObject> pointsWaitingList = new List<GameObject>();
+    float health = 100;
+    float redHealth = 100;
+    Coroutine healthPicCoroutine;
+    bool healthPicCorIsRunning;
+    Coroutine redHealthCoroutine;
+    bool redHealthCorIsRunning;
+
 
     #region Public Update Visuals Functions
     public void UpdateHealth(float currentHealth)
     {
-        //Set pic and bg
-        if (currentHealth < healthPicChangeThreshhold)
-        {
-            healthBG.sprite = healthBGs[1];
-            healthPic.sprite = healthPics[1];
-            healthSlider.color = lowHealthBar;
-        }
-        else
-        {
-            healthBG.sprite = healthBGs[0];
-            healthPic.sprite = healthPics[0];
-            healthSlider.color = Color.white;
-        }
-
         //Slider
         float relativeHP = currentHealth / 100;
         healthSlider.fillAmount = relativeHP;
+
+        if (redHealthCorIsRunning)
+        {
+            StopCoroutine(redHealthCoroutine);
+        }
+        redHealthCoroutine = StartCoroutine(RedHealthAnimation (currentHealth));
+
+        //Damaged or healed
+        if (health > currentHealth)
+        {
+            
+            health = currentHealth;
+
+            if (healthPicCorIsRunning)
+            {
+                StopCoroutine(healthPicCoroutine);
+            }
+            //Set pic and bg
+            if (currentHealth < healthPicChangeThreshhold)
+            {
+                healthBG.sprite = healthBGs[1];
+                healthPic.sprite = healthPics[1];
+                healthSlider.color = lowHealthBar;
+            }
+            else
+            {
+                
+                healthPicCoroutine = StartCoroutine(HealthPicAnimation());
+
+            }
+        }
+        else
+        {
+            if (currentHealth < healthPicChangeThreshhold)
+            {
+                healthBG.sprite = healthBGs[1];
+                healthPic.sprite = healthPics[1];
+                healthSlider.color = lowHealthBar;
+            }
+            else
+            {
+                healthBG.sprite = healthBGs[0];
+                healthPic.sprite = healthPics[0];
+                healthSlider.color = Color.white;
+            }
+        }
+
+    }
+
+    private IEnumerator RedHealthAnimation(float currentHealth)
+    {
+        print(currentHealth);
+        redHealthCorIsRunning = true;
+        yield return new WaitForSeconds(healthAnimTime);
+
+        float t = 0;
+        while (healthSliderRed.fillAmount != healthSlider.fillAmount)
+        {
+            t += Time.deltaTime;
+            float relativeRedHealth = Mathf.Lerp(redHealth / 100, currentHealth / 100, redHealthAnimCurve.Evaluate(t * redHealthAnimSpeed));
+            healthSliderRed.fillAmount = relativeRedHealth;
+            redHealth = relativeRedHealth * 100;
+            yield return null;
+        }
+        redHealthCorIsRunning = true;
+    }
+
+    IEnumerator HealthPicAnimation()
+    {
+        healthPicCorIsRunning = true;
+        healthBG.sprite = healthBGs[1];
+        healthPic.sprite = healthPics[2];
+        yield return new WaitForSeconds(healthAnimTime);
+        healthBG.sprite = healthBGs[0];
+        healthPic.sprite = healthPics[0];
+        healthSlider.color = Color.white;
+        healthPicCorIsRunning = false;
 
     }
 
@@ -143,7 +220,7 @@ public class UIManager : MonoBehaviour
 
     public void UpdateMultiplier(int multiplier)
     {
-        scoreMultiplier.text = multiplier.ToString() + "X";
+        scoreMultiplier.text = multiplier.ToString();
     }
 
     public void UpdateObjectives(string mainObjective, string bonusObjective, int bonusPointsAmount)
@@ -154,4 +231,10 @@ public class UIManager : MonoBehaviour
     }
 
     #endregion
+
+    public void DebugRedHealth(float dmg)
+    {
+        float newHealth = Mathf.Clamp(health - dmg, 0, 100);
+        UpdateHealth(newHealth);
+    }
 }
